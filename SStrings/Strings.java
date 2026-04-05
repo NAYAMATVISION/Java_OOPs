30e9d58eb415c477e2586d0ddf7ffcf154f0d427
NAYAMATVISION <nayamatemeet@gmail.com>
2026-04-05 11:47:30 +0530

    Strings

---
+++ SStrings/Strings.java
@@ -0,0 +1,24 @@
+package SStrings;
+
+//import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println(name);
+        // System.out.println(name.length());
+
+        //charAt
+        // for(int i=0;i<name.length();i++){
+        //     System.out.print(name.charAt(i) + " ");
+        // }
+        // String name1 = "nayu";
+        // String name2 = "aayu";
+        // System.out.println(name1.compareTo(name2));
+        // sc.close();
+        String sentence = "I am nayu";
+        String name = sentence.substring(5,sentence.length());
+        System.out.println(name);
+    }
+}
