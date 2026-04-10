b107a84928fa22b71e309c977af2e1bd1003854a
NAYAMATVISION <nayamatemeet@gmail.com>
2026-04-10 19:31:15 +0530

    ArrayList

---
+++ JavaCollectionFramework/A.java
@@ -0,0 +1,7 @@
+package JavaCollectionFramework;
+
+public class A {
+    void funA(){
+        System.out.println("funA is getting called");
+    }
+}
---
+++ JavaCollectionFramework/ListArray.java
@@ -0,0 +1,38 @@
+package JavaCollectionFramework;
+import java.util.*;
+public class ListArray {
+    public static void main(String[] args) {
+        // ArrayList<Integer> al = new ArrayList<>(); //intitial capacity 10
+        // for(int i=0;i<10;i++){
+        //     al.add(i);
+        // }
+        // System.out.println(al);
+        // how it is supporting primitives ? 
+        // this is the concept of autoboxing and auto unboxing
+        // int ----> Integer(wraper class of int basically this is ki int ko Integer class me baandh diya)
+
+        // int i = 10;
+        // Integer i1 = Integer.valueOf(i); // boxing
+        // int x = i1.intValue(); //unboxing
+        // System.out.println(x);
+        // manual boxing // manual unboxing
+        //al.add("hi"); now al is not raw array list
+
+        // ArrayList al = new ArrayList();
+        // al.add(100);
+        // al.add('a');
+        // al.add("hi");
+        // al.add(new A());
+        // System.out.println(al);
+        // //System.out.println(al.get(3).funA());
+        // Object obj = al.get(3);
+        // A a1 = (A)obj;
+        // a1.funA();
+
+
+
+
+
+
+    }
+}
